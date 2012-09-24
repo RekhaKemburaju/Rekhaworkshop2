72897b5f7395cc81feea9e7343f6d96a1afc7604
RekhaKemburaju <rekha.kemburaju@gmail.com>
2012-09-24 12:15:20 +0800

    i know something

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@
 // $("img").width(200);
 $(document).ready(function(){
 // write all ur jQuery codes inside here.
+	$("li").on("click",function(){
+
+		if($(this).hasClass("done")){
+			$(this).removeClass("done");
+		}else{
+			$(this).addClass("done");
+		}
+		//$(this).addClass("done");
+		//$("li").addClass("done");
+	});
 
 	$("img").on("dblclick", function(){ // this is more better then the one bellow more powerful
 
 		$("img").width(200);
 		$("h1").fadeOut();
 		$(".booboo").slideUp();
-		//$("#rahul").hide();
+		$("#rahul").hide();
 
 	});
 
